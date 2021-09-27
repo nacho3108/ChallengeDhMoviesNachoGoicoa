e9570bd2844a34c4efdedbd0cdca62cff840ffc8
Juan Ignacio Goicoa <juanignaciogoicoa@MBP-de-Juan.fibertel.com.ar>
2021-09-27 19:33:21 -0300

    Meoras en vistas y funciones

--- Challenge nodejs - backend/database/config/config.js
+++ Challenge nodejs - backend/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "root",
+    "password": "",
     "database": "movies_db",
     "host": "127.0.0.1",
     "dialect": "mysql",
@@ -9,14 +9,14 @@ module.exports = {
   },
   "test": {
     "username": "root",
-    "password": "root",
+    "password": "",
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": "root",
+    "password": "",
     "database": "database_production",
     "host": "127.0.0.1",
     "dialect": "mysql"
